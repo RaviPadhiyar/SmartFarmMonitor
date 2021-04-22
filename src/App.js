61ee7f8606342b114f4cc5ead42463c37de2325c
RaviPadhiyar <rvpadhiyar30@gmail.com>
2021-04-22 13:01:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" />
-      <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line" />
-      <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line" />
+      <iframe title='moisture' width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" />
+      <iframe title='humidity' width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line" />
+      <iframe title='temp' width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line" />
       <hr/>
       <hr/>
       <div className='container'>
