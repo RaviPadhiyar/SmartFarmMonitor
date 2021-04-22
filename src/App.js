5ff7c6951b9948d279e795bdcc46335f380d20a2
RaviPadhiyar <rvpadhiyar30@gmail.com>
2021-04-22 12:45:04 +0530

    second commmit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" />
+      <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line" />
+      <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line" />
+      <hr/>
+      <div className='container'>
+        <h2>Current Reading</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>Smart Farm Monitoring System</h1>
+        </header>
+    )
+}
+
+export default Header
