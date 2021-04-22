d78f9fb7b2197f894a5b0ca00267fc7172e64d7e
RaviPadhiyar <rvpadhiyar30@gmail.com>
2021-04-22 12:50:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line" />
       <iframe width={450} height={260} style={{border: '1px solid #cccccc'}} src="https://thingspeak.com/channels/1349067/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line" />
       <hr/>
+      <hr/>
       <div className='container'>
         <h2>Current Reading</h2>
       </div>
